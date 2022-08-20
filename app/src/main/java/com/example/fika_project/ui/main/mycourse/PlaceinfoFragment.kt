248e25a9e5c37704d75a52caafa0d249245c6245
yejinyun <73897939+yejinyun@users.noreply.github.com>
2022-08-21 01:01:07 +0900

    레이아웃 11 모음집 추가

---
+++ app/src/main/java/com/example/fika_project/ui/main/mycourse/PlaceinfoFragment.kt
@@ -0,0 +1,35 @@
+package com.example.fika_project.ui.main.mycourse
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ScrollView
+import androidx.fragment.app.Fragment
+import com.example.fika_project.databinding.FragmentPlaceinfoBinding
+import com.example.fika_project.databinding.FragmentPlaceinfoLocateBinding
+import kotlinx.android.synthetic.main.fragment_placeinfo_locate.*
+
+class PlaceinfoFragment: Fragment() {
+    private var _binding: FragmentPlaceinfoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentPlaceinfoBinding.inflate(inflater, container, false)
+
+        initView()
+
+        return binding.root
+    }
+
+    private fun initView() {
+        binding.placeinfoUpBtn.setOnClickListener {
+            binding.placeinfoSv.fullScroll(ScrollView.FOCUS_UP)
+        }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
