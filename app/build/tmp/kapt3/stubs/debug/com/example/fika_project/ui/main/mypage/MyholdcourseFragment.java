4a83f79bb0d3459da78dc3fde097997d80ecf964
Ojongseok <whdtjr6889@naver.com>
2022-08-31 20:40:33 +0900

    08.31 20:36

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/fika_project/ui/main/mypage/MyholdcourseFragment.java
@@ -0,0 +1,50 @@
+package com.example.fika_project.ui.main.mypage;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000T\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u00012\u00020\u0002B\u0005\u00a2\u0006\u0002\u0010\u0003J\b\u0010\r\u001a\u00020\u000eH\u0002J&\u0010\u000f\u001a\u0004\u0018\u00010\u00102\u0006\u0010\u0011\u001a\u00020\u00122\b\u0010\u0013\u001a\u0004\u0018\u00010\u00142\b\u0010\u0015\u001a\u0004\u0018\u00010\u0016H\u0016J\u0018\u0010\u0017\u001a\u00020\u000e2\u0006\u0010\u0018\u001a\u00020\u00192\u0006\u0010\u001a\u001a\u00020\u001bH\u0016J\b\u0010\u001c\u001a\u00020\u000eH\u0016J\u0010\u0010\u001d\u001a\u00020\u000e2\u0006\u0010\u001e\u001a\u00020\u001fH\u0016R\u0010\u0010\u0004\u001a\u0004\u0018\u00010\u0005X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0006\u001a\u00020\u00058BX\u0082\u0004\u00a2\u0006\u0006\u001a\u0004\b\u0007\u0010\bR\u0011\u0010\t\u001a\u00020\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\f\u00a8\u0006 "}, d2 = {"Lcom/example/fika_project/ui/main/mypage/MyholdcourseFragment;", "Landroidx/fragment/app/Fragment;", "Lcom/example/fika_project/ui/main/mypage/myspot/MySpotView;", "()V", "_binding", "Lcom/example/fika_project/databinding/FragmentMyholdcourseBinding;", "binding", "getBinding", "()Lcom/example/fika_project/databinding/FragmentMyholdcourseBinding;", "service", "Lcom/example/fika_project/ui/main/mypage/myspot/MySpotService;", "getService", "()Lcom/example/fika_project/ui/main/mypage/myspot/MySpotService;", "onClickListener", "", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onExploreFailure", "code", "", "message", "", "onExploreLoading", "onExploreSuccess", "response", "Lcom/example/fika_project/ui/main/mypage/myspot/MySpotResponse;", "app_debug"})
+public final class MyholdcourseFragment extends androidx.fragment.app.Fragment implements com.example.fika_project.ui.main.mypage.myspot.MySpotView {
+    private com.example.fika_project.databinding.FragmentMyholdcourseBinding _binding;
+    @org.jetbrains.annotations.NotNull()
+    private final com.example.fika_project.ui.main.mypage.myspot.MySpotService service = null;
+    private java.util.HashMap _$_findViewCache;
+    
+    public MyholdcourseFragment() {
+        super();
+    }
+    
+    private final com.example.fika_project.databinding.FragmentMyholdcourseBinding getBinding() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.fika_project.ui.main.mypage.myspot.MySpotService getService() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    @java.lang.Override()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    private final void onClickListener() {
+    }
+    
+    @java.lang.Override()
+    public void onExploreLoading() {
+    }
+    
+    @java.lang.Override()
+    public void onExploreSuccess(@org.jetbrains.annotations.NotNull()
+    com.example.fika_project.ui.main.mypage.myspot.MySpotResponse response) {
+    }
+    
+    @java.lang.Override()
+    public void onExploreFailure(int code, @org.jetbrains.annotations.NotNull()
+    java.lang.String message) {
+    }
+}
